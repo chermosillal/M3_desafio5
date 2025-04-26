753dfc689b4d165c017cae78a5d047f86669d18a
CesarH <cesar.hermosilla@gmail.com>
2025-04-26 01:12:00 -0400

    Subida Inicial

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let total = document.querySelector("#total"); 
+//total.textContent =20;
+total.innerHTML=20;
+console.log(total);
+
+
+let realizadas = document.querySelector("#realizadas"); 
+console.log(realizadas);
+realizadas.innerHTML=30;
+
+const tareas = document.querySelector("#tarea");
+tareas.innerHTML="<tr><td>8</td><td>8</td><td>8</td><td>8</td></tr>"
+// tareas.innerHTML="<tbody id='tarea'><tr><td>5</td><td>5</td><td>5</td><td>5</td></tr></tbody>"
+console.log(tareas);
